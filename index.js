375ca3a8ef46d97192da41a51d48dc1c912f34a1
Deval <deval135darji@gmail.com>
2023-09-26 16:36:29 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const db = require("./connection");
@@ -6,23 +6,34 @@ const userRout = require("./userRouter");
 const empRout = require("./empRouter");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(userRout);
 app.use(empRout);
 
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
-app.get('/userLogin', (req, res) => res.sendFile(__dirname + "/userLogin.html"))
-app.get('/empLogin', (req, res) => res.sendFile(__dirname + "/empLogin.html"))
-app.get('/usersignup', (req, res) => res.sendFile(__dirname + "/usersignup.html"))
-app.get('/booking', (req, res) => res.sendFile(__dirname + "/booking.html"))
-app.get('/emppassinfo', (req, res) => res.sendFile(__dirname + "/passenger-info.html"))
-app.get('/manageflight', (req, res) => res.sendFile(__dirname + "/flight.html"))
-app.get('/userhelp', (req, res) => res.sendFile(__dirname + "/help.html"))
-app.get('/addflight', (req, res) => res.sendFile(__dirname + "/addflight.html"))
-app.get('/deleteflight', (req, res) => res.sendFile(__dirname + "/deleteflight.html"))
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/userLogin", (req, res) =>
+  res.sendFile(__dirname + "/userLogin.html")
+);
+app.get("/empLogin", (req, res) => res.sendFile(__dirname + "/empLogin.html"));
+app.get("/usersignup", (req, res) =>
+  res.sendFile(__dirname + "/usersignup.html")
+);
+app.get("/booking", (req, res) => res.sendFile(__dirname + "/booking.html"));
+app.get("/emppassinfo", (req, res) =>
+  res.sendFile(__dirname + "/passenger-info.html")
+);
+app.get("/manageflight", (req, res) =>
+  res.sendFile(__dirname + "/flight.html")
+);
+app.get("/userhelp", (req, res) => res.sendFile(__dirname + "/help.html"));
+app.get("/addflight", (req, res) =>
+  res.sendFile(__dirname + "/addflight.html")
+);
+app.get("/deleteflight", (req, res) =>
+  res.sendFile(__dirname + "/deleteflight.html")
+);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
